3609940a2f083dde40c325c0b87fb40d51f6c509
DreamMaoMao <2523610504@qq.com>
2025-04-28 21:20:51 +0800

    u

--- maomao/autostart.sh
+++ maomao/autostart.sh
@@ -3,32 +3,17 @@
 
 set +e
 
-
-# systemctl --user unmask xdg-desktop-portal-hyprland
-# systemctl --user mask xdg-desktop-portal-gnome
-
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
 
-# /usr/lib/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-wlr &
 
 swaync &
 wlsunset -T 3501 -t 3500 &
 swaybg -i ~/.config/maomao/wallpaper/room.png &
 waybar -c ~/.config/maomao/waybar/config -s ~/.config/maomao/waybar/style.css &
-echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
-# 开启输入法
-fcitx5 --replace -d &
 
-# wl-clip-persist --clipboard regular &
+wl-clip-persist --clipboard regular --reconnect-tries 0 &
 wl-paste --type text --watch cliphist store & 
-blueman-applet &
-nm-applet &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-[ -e /dev/sda4 ] && udisksctl mount -t ext4 -b /dev/sda4
-cp ~/.config/eww/System-Menu/eww.yuck.hyprland  ~/.config/eww/System-Menu/eww.yuck
-eww daemon &
 
-~/.config/hypr/scripts/idle.sh &
-sway-audio-idle-inhibit &
-swayosd-server &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
